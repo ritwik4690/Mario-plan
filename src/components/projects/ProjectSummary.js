d6f5f3cec9ca37aa2ca8a7c389b40af442d1e08d
ritwik4690 <vaidyaritwik@gmail.com>
2021-02-13 17:16:05 +0530

    Time added

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,3 +1,4 @@
+import moment from "moment";
 import React from "react";
 import projectReducer from "../../store/reducers/projectReducer";
 
@@ -6,8 +7,12 @@ const ProjectSummary = ({ project }) => {
     <div className="card z-depth-0 project-summary">
       <div className="card-content grey-text text-darken-3">
         <span className="card-title">{project.title}</span>
-        <p>Posted by {project.authorFirstName} {project.authorLastName}</p>
-        <p className="grey-text">3rd September, 2am</p>
+        <p>
+          Posted by {project.authorFirstName} {project.authorLastName}
+        </p>
+        <p className="grey-text">
+          {moment(project.createdAt.toDate()).calendar()}
+        </p>
       </div>
     </div>
   );
